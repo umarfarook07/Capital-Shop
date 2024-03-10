ae6da9c047100a2d468582e500f2469bee09c4d5
umarfarook07 <154865642+umarfarook07@users.noreply.github.com>
2024-03-10 17:24:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './Footer/Footer.jsx';
 import "./Styles/responsive.css";
 import SwiperMenu from './Swiper/SwiperMenu.jsx';
 import "./Styles/reset.css";
+import BenefitsSection from './BenefitsSection.jsx'
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
       <Menu />
       <SwiperMenu />
       <HeroSection />
+      <BenefitsSection />
       <Footer />
     </>
   );
