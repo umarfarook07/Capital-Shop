9c88564da08f58182043024add717be0b54e11a8
umarfarook07 <154865642+umarfarook07@users.noreply.github.com>
2024-03-14 12:13:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Header from './Header/Header.jsx';
-import Menu from './Menu/Menu.jsx';
-import HeroSection from './Hero-Section/HeroSection.jsx';
 import Footer from './Footer/Footer.jsx';
+import Home from './Home/Home.jsx';
 import "./Styles/responsive.css";
-import SwiperMenu from './Swiper/SwiperMenu.jsx';
 import "./Styles/reset.css";
-import BenefitsSection from './Benefits-Section/BenefitsSection.jsx'
-import NewsletterSubscription from './NewsletterSubscription/NewsletterSubscription.jsx'
+
+// Create a browser router instance
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  // You can add more routes here
+]);
 
 function App() {
   return (
     <>
       <Header />
-      <Menu />
-      <SwiperMenu />
-      <HeroSection />
-      <BenefitsSection />
-      <NewsletterSubscription />
+      <RouterProvider router={router} />
       <Footer />
     </>
   );
 }
 
-export default App
+export default App;
