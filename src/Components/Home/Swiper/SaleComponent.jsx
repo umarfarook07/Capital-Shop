0dee68f71997bb3c09a2e075ed425aaa4d76f957
umarFarook <umarkhaji456@gmail.com>
2024-03-17 15:41:19 +0530

    added mobile responsiveness

--- src/Components/Home/Swiper/SaleComponent.jsx
+++ src/Components/Home/Swiper/SaleComponent.jsx
@@ -3,12 +3,12 @@ import './SaleComponent.css'
 
 const SaleComponent = () => {
   return (
-    <>
+    <div className='sale-component'>
       <h2>Exclusive Season Sale</h2>
       <h1>Up to 50% Off on Men's Wear</h1>
       <p>Discover our latest collection of men's fashion items with incredible discounts.</p>
       <button>SHOP NOW</button>
-    </>
+    </div>
   )
 }
 
