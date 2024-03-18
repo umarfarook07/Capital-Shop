a34bb501878038b3bb27430f2fd3ef90cf8bb2b1
umarFarook <umarkhaji456@gmail.com>
2024-03-18 22:53:37 +0530

    added collection page

--- src/Components/Collections/MensCollection.jsx
+++ src/Components/Collections/MensCollection.jsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 
+export const Item = ({ category, SrcImg }) => {
+  return (
+    <div>
+      <div className='category-item'>Category: {category}</div>
+      <img src={SrcImg} alt={category} />
+    </div>
+  );
+};
+
 function MensCollection() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch('http://localhost:3001/api/products'); 
+        const response = await fetch('http://localhost:3001/api/products');
         if (!response.ok) throw new Error('Data could not be fetched!');
         const data = await response.json();
         setProducts(data);
@@ -16,14 +25,16 @@ function MensCollection() {
     };
 
     fetchProducts();
-  }, []); 
+  }, []);
 
   return (
     <div>
-      <h2>Products</h2>
-      <ul>
+      <h2>Shop By Category</h2>
+      <ul className='category-list'>
         {products.map(product => (
-          <li key={product._id}>{product.name} - ${product.price}</li> 
+          <li key={product.id}>
+            <Item category={product.category} SrcImg={product.img} />
+          </li>
         ))}
       </ul>
     </div>
